0a1b35e6eeab54f35587f46f9da9df2c42386be0
wagnerk78 <wagnerkroiss@gmail.com>
2023-10-10 16:27:30 -0300

    First Commit

---
+++ src/main/java/br/com/wagner/todolist/users/UserModel.java
@@ -0,0 +1,29 @@
+package br.com.wagner.todolist.users;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+
+@Data
+@Entity(name = "tb_users")
+public class UserModel {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
+
+    @Column(unique = true)
+    private String username;
+    private String password;
+    private String name;
+
+    @CreationTimestamp
+    private LocalDateTime CreatedAt;
+}
